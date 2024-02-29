2db113dc2a5d59fa07f1c3a4b236ce7f1ba43d53
sasi-reddy <bvsasidharr@gmail.com>
2024-02-29 14:06:48 +0530

    updated

--- interface.java
+++ interface.java
@@ -1,9 +1,23 @@
+import java.util.*;
 public interface sum{
-	public static int sum(int n1,int n2);
+	public  int sum(int n1,int n2);
 }
 
 class sumimpl implements sum{
-	public static int sum(int n1,int n2){
+	public  int sum(int n1,int n2){
 		return n1+n2;
+	}
+}
+
+class main{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter number to sum");
+
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		sumimpl s=new sumimpl();
+
+		System.out.println(n1+"+"+n2+"="+s.sum(n1,n2));
 	}
 }
